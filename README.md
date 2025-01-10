6564c54ee07346c9ed428727bdda4c2af769b30a
Saqib Nasir <msaqibtech1@gmail.com>
2025-01-10 20:08:35 +0500

    portfolio

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# portfolio animations
- 
+# Persnol_Portfolio_website_design
+    This Is My Portfolio Website.
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ==================== typing Animations =============================
+
+var typed = new Typed(".typing",{
+    strings:["","Web Developer", "Graphic Designer", "Beckend Developer", "Content Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
